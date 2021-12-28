0c96fbced1e7f4821881a0a5bffb2558225d932c
Stefan Lendl <ste.lendl@gmail.com>
2021-12-28 10:18:03 +0100

    move packages.el definitions to config.org

--- packages.el
+++ packages.el
@@ -1,118 +1,27 @@
-;; -*- no-byte-compile: t; -*-
-;;; $DOOMDIR/packages.el
-
-;; To install a package with Doom you must declare them here, run 'doom sync' on
-;; the command line, then restart Emacs for the changes to take effect.
-;; Alternatively, use M-x doom/reload.
-;;
-;; WARNING: Disabling core packages listed in ~/.emacs.d/core/packages.el may
-;; have nasty side-effects and is not recommended.
-
-
-;; All of Doom's packages are pinned to a specific commit, and updated from
-;; release to release. To un-pin all packages and live on the edge, do:
-;(unpin! t)
-
-;; ...but to unpin a single package:
-;(unpin! pinned-package)
-;; Use it to unpin multiple packages
-;(unpin! pinned-package another-pinned-package)
-
-
-;; To install SOME-PACKAGE from MELPA, ELPA or emacsmirror:
-;(package! some-package)
-
-;; To install a package directly from a particular repo, you'll need to specify
-;; a `:recipe'. You'll find documentation on what `:recipe' accepts here:
-;; https://github.com/raxod502/straight.el#the-recipe-format
-;(package! another-package
-;  :recipe (:host github :repo "username/repo"))
-
-;; If the package you are trying to install does not contain a PACKAGENAME.el
-;; file, or is located in a subdirectory of the repo, you'll need to specify
-;; `:files' in the `:recipe':
-;(package! this-package
-;  :recipe (:host github :repo "username/repo"
-;           :files ("some-file.el" "src/lisp/*.el")))
-
-;; If you'd like to disable a package included with Doom, for whatever reason,
-;; you can do so here with the `:disable' property:
-;(package! builtin-package :disable t)
-
-;; You can override the recipe of a built in package without having to specify
-;; all the properties for `:recipe'. These will inherit the rest of its recipe
-;; from Doom or MELPA/ELPA/Emacsmirror:
-;(package! builtin-package :recipe (:nonrecursive t))
-;(package! builtin-package-2 :recipe (:repo "myfork/package"))
-
-;; Specify a `:branch' to install a package from a particular branch or tag.
-;; This is required for some packages whose default branch isn't 'master' (which
-;; our package manager can't deal with; see raxod502/straight.el#279)
-;(package! builtin-package :recipe (:branch "develop"))
-
-;; (package! lsp-treemacs)
-(package! ztree)
-;; (package! fira-code-mode)
-;; (package! todoist)
-(package! org-jira)
-;; (package! ejira
-;;   :recipe (:host github
-;;            :repo "nyyManni/ejira"
-;;            :files ("*.el")))
-
-;; Roam Packages
-;; (package! org-roam-server)
-;; (package! company-org-roam)
-;; (package! org-roam)
-
-;; (package! helm-org-rifle)
-
 (package! org-super-agenda)
+
 (package! org-ql)
-(package! org-web-tools)
-(package! ob-mermaid)
 
-;; (package! org-gcal)
-
-;; Orgmode Packages
-(package! org-analyzer) ;; needs java
 (package! org-edna)
 
 (package! websocket)
-(package! org-roam-ui :recipe (:host github :repo "org-roam/org-roam-ui" :files ("*.el" "out")))
+(package! org-roam-ui
+  :recipe (:host github
+           :repo "org-roam/org-roam-ui"
+           :files ("*.el" "out")))
 
-;; (package! zp-org-agenda
-;;   :recipe (:host github
-;;            :repo "zaeph/.emacs.d"
-;;            :files ("lisp/*.el")
-;;            ))
+(package! ob-mermaid)
 
-;; (package! org-linker-edna :recipe (:host github :repo "toshism/org-linker-edna"))
-;; (package! org-linker :recipe (:host github :repo "toshism/org-linker"))
+(package! org-jira)
 
-;; (package! gitlab-ci-mode)
-;; (package! gitlab-ci-mode-flycheck)
-
-
-;; mu4e
-;; (package! mu4e-views)
-
-;; (package! with-editor)
-
-;; folding
-;; (package! origami)
-
-;; kubernetes
 (package! kubernetes)
 (package! kubernetes-evil)
 (package! kubernetes-helm)
 (package! k8s-mode)
 
-(package! edbi)
-(package! edbi-minor-mode)
-;; has some dependencies
-;; https://github.com/kiwanami/emacs-edbi#installation
 (package! sql-indent)
 
-;; (package! tree-sitter)
-;; (package! tree-sitter-langs)
+(package! edbi)
+(package! edbi-minor-mode)
+
+(package! ztree)
