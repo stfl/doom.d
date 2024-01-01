0e9136c6fe6271e06ec12f12aa4531c6ab742583
Stefan Lendl <s.lendl@proxmox.com>
2024-01-01 22:27:14 +0100

    changes from amsel on Mon Jan 1 10:27:13 PM CET 2024

--- config.el
+++ config.el
@@ -1326,21 +1326,6 @@ org-default-priority is treated as lower than the same set value"
 
 (after! org (setq org-indent-indentation-per-level 2))
 
-;; (after! org-superstar
-;;   ;; Every non-TODO headline now have no bullet
-;;   ;; (setq org-superstar-headline-bullets-list '("　"))
-;;   (setq org-superstar-leading-bullet ?　)
-;;   ;; Enable custom bullets for TODO items
-;;   (setq org-superstar-special-todo-items t)
-;;   (setq org-superstar-todo-bullet-alist
-;;         '(("TODO" "☐")
-;;           ("NEXT" "➡")
-;;           ("PROJ" "⎚")
-;;           ("WAIT" "⏳")
-;;           ("KILL" "✘")
-;;           ("DONE" "✔")))
-;;   (org-superstar-restart))
-
 (after! org (setq org-log-state-notes-insert-after-drawers nil))
 
 (after! org
