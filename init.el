b372dd92888c740cce61a96f1f34912ae869fd6e
Stefan Lendl <s.lendl@proxmox.com>
2024-01-03 09:26:52 +0100

    changes from leah on Wed Jan 3 09:26:52 AM CET 2024

--- init.el
+++ init.el
@@ -128,7 +128,7 @@
        (magit             ; a git porcelain for Emacs
         +forge)
        ;;make              ; run make tasks from Emacs
-       ;; (pass +auth)        ; password manager for nerds
+       (pass +auth)        ; password manager for nerds
        pdf               ; pdf enhancements
        ;;prodigy           ; FIXME managing external services & code builders
        rgb               ; creating color strings
