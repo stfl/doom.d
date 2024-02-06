365882474e91572600e734602da69c27b14041df
Stefan Lendl <ste.lendl@gmail.com>
2024-02-06 14:52:24 +0100

    changes from leah on Tue Feb 6 02:52:24 PM CET 2024

--- init.el
+++ init.el
@@ -44,7 +44,7 @@
        hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        hydra
                                         ; (:cond ((unless (string= system-name "stefan-t3600")
-       ;; indent-guides     ; highlighted indent columns
+       indent-guides     ; highlighted indent columns
                                         ; )))
        ;; minimap           ; show a map of the code on the side
        modeline          ; snazzy, Atom-inspired modeline, plus API
