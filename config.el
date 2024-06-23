0447a11502acfbd0b58a8aae4c0d870622f8af17
Stefan Lendl <git@stfl.dev>
2024-06-23 22:22:27 +0200

    changes from kondor on So 23 Jun 2024 22:22:27 CEST

--- config.el
+++ config.el
@@ -287,6 +287,7 @@
       :map org-mode-map
       :localleader
       :desc "Revert all org buffers" "R" #'org-revert-all-org-buffers
+      "F" #'+org-fix-blank-lines
       "N" #'org-add-note
 
       :prefix ("s" . "Tree/Subtree")
@@ -450,8 +451,6 @@ relevant again (Tickler)"
 (after! org
 
 (setq stfl/agenda-backlog-prio-threshold (+ 2 org-priority-default))
-;; TODO I really don't know what this is used for? this might be an old variable.
-;; It's still used tho for a basiclly unused custom-command!
 
 (setq-default stfl/agenda-max-prio-group ?D)
 ;; Priority level until the backlog in today-agenda is shown!
@@ -582,6 +581,7 @@ relevant again (Tickler)"
                               (not (scheduled))
                               (primary-work))
                         ((org-ql-block-header "Next Actions")
+                         (stfl/agenda-max-prio-group org-default-priority)
                          (org-super-agenda-groups stfl/ancestor-priority-groups)))
           (org-ql-block '(and (stuck-proj)
                               (primary-work))
@@ -1769,6 +1769,44 @@ Not added when either:
 (after! org-tree-slide
   (remove-hook 'org-tree-slide-play-hook #'+org-present-hide-blocks-h)
   (remove-hook 'org-tree-slide-stop-hook #'+org-present-hide-blocks-h))
+
+;;;###autoload
+(defun +org-fix-blank-lines (&optional prefix)
+  "Ensure that blank lines exist between headings and between headings and their contents.
+With prefix, operate on whole buffer. Ensures that blank lines
+exist after each headings's drawers."
+  (interactive "P")
+  (org-map-entries (lambda ()
+                     (org-with-wide-buffer
+                      ;; `org-map-entries' narrows the buffer, which prevents us from seeing
+                      ;; newlines before the current heading, so we do this part widened.
+                      (while (not (looking-back "\n\n" nil))
+                        ;; Insert blank lines before heading.
+                        (insert "\n")))
+                     (let ((end (org-entry-end-position)))
+                       ;; Insert blank lines before entry content
+                       (forward-line)
+                       (while (and (org-at-planning-p)
+                                   (< (point) (point-max)))
+                         ;; Skip planning lines
+                         (forward-line))
+                       (while (re-search-forward org-drawer-regexp end t)
+                         ;; Skip drawers. You might think that `org-at-drawer-p' would suffice, but
+                         ;; for some reason it doesn't work correctly when operating on hidden text.
+                         ;; This works, taken from `org-agenda-get-some-entry-text'.
+                         (re-search-forward "^[ \t]*:END:.*\n?" end t)
+                         (goto-char (match-end 0)))
+                       (unless (or (= (point) (point-max))
+                                   (org-at-heading-p)
+                                   (looking-at-p "\n"))
+                         (insert "\n"))))
+                   t (if prefix
+                         nil
+                       'tree))
+  (message "fixed blank lines in org buffer"))
+
+(add-hook! org-mode-hook
+  (add-hook! before-save-hook :local #'+org-fix-blank-lines))
 
 (use-package! define-word
   :after org
