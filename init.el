17d46075d977a48254fa00267db78c2c3a9e6577
Stefan Lendl <git@stfl.dev>
2024-07-17 21:31:52 +0200

    changes from falke on Mi 17 Jul 2024 21:31:52 CEST

--- init.el
+++ init.el
@@ -131,7 +131,6 @@
        (pass +auth)        ; password manager for nerds
        pdf               ; pdf enhancements
        ;;prodigy           ; FIXME managing external services & code builders
-       rgb               ; creating color strings
        ;;taskrunner        ; taskrunner for all your projects
        ;;terraform         ; infrastructure as code
        ;;tmux              ; an API for interacting with tmux
