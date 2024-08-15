3dda1838944d7200a6d3edb69b7a2aa46f298445
Stefan Lendl <git@stfl.dev>
2024-08-15 22:51:20 +0200

    changes from falke on Do 15 Aug 2024 22:51:20 CEST

--- packages.el
+++ packages.el
@@ -28,10 +28,10 @@
 
 (package! emacsql-sqlite-builtin)
 
-;; (package! copilot
-;;   :recipe (:host github
-;;            :repo "zerolfx/copilot.el"
-;;            :files ("*.el" "dist")))
+(package! copilot
+  :recipe (:host github
+           :repo "zerolfx/copilot.el"
+           :files ("*.el" "dist")))
 
 (package! codeium
   :recipe (:host github
