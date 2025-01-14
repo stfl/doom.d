69efbbaff81faaabe3b5d7c52075f59aed9a45d4
Stefan Lendl <git@stfl.dev>
2025-01-14 13:50:39 +0100

    changes from kondor on Di 14 Jän 2025 13:50:39 CET

--- config.el
+++ config.el
@@ -1888,7 +1888,8 @@ org-default-priority is treated as lower than the same set value"
 
 (after! vterm
   (setq! vterm-max-scrollback 200000
-         vterm-min-window-width 5000)) ;; do not wrap long lines per default
+         ;; vterm-min-window-width 5000
+         )) ;; do not wrap long lines per default
 
 (map!
  :after vterm
