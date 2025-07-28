547944f24b995474fefddf86d0eb028f7353ed71
Stefan Lendl <git@stfl.dev>
2025-07-28 21:25:23 +0200

    changes from kondor on Mo 28 Jul 2025 21:25:23 CEST

--- packages.el
+++ packages.el
@@ -32,13 +32,13 @@
            :repo "meow_king/typst-ts-mode"
            :files (:defaults "*.el")))
 
-(package! lsp-bridge
-  :recipe (:host github
-           :repo "manateelazycat/lsp-bridge"
-           :branch "master"
-           :files ("*.el" "*.py" "acm" "core" "langserver" "multiserver" "resources")
-           ;; do not perform byte compilation or native compilation for lsp-bridge
-           :build (:not compile)))
+;; (package! lsp-bridge
+;;   :recipe (:host github
+;;            :repo "manateelazycat/lsp-bridge"
+;;            :branch "master"
+;;            :files ("*.el" "*.py" "acm" "core" "langserver" "multiserver" "resources")
+;;            ;; do not perform byte compilation or native compilation for lsp-bridge
+;;            :build (:not compile)))
 
 ;; (package! numpydoc)
 
