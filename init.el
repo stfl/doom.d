8705eca6a3ff0cb5e833844f2b8ba204611fb3e8
Stefan Lendl <git@stfl.dev>
2025-08-04 23:19:02 +0200

    changes from kondor on Mo 04 Aug 2025 23:19:01 CEST

--- init.el
+++ init.el
@@ -129,7 +129,8 @@
         )
        direnv
        (docker
-        +lsp)
+        ;; +lsp
+        )
        editorconfig      ; let someone else argue about tabs vs spaces
        ;; ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)     ; run code, run (also, repls)
