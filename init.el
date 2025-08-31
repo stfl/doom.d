3f8cf6bcae984a7784a2fbb62baecea47eaf4924
Stefan Lendl <git@stfl.dev>
2025-08-31 20:17:41 +0200

    changes from kondor on So 31 Aug 2025 20:17:41 CEST

--- init.el
+++ init.el
@@ -236,7 +236,8 @@
        ;;ruby              ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
        (rust               ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
         +lsp
-        +tree-sitter)
+        ;; +tree-sitter
+        )
        ;;scala             ; java, but good
        ;;scheme            ; a fully conniving family of lisps
        (sh                 ; she sells {ba,z,fi}sh shells on the C xor
