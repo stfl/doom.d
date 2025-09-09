ff574f1e1c105943da57d0478950756c0557bcf1
Stefan Lendl <git@stfl.dev>
2025-09-09 09:35:10 +0200

    changes from kondor on Di 09 Sep 2025 09:35:10 CEST

--- packages.el
+++ packages.el
@@ -4,11 +4,11 @@
 
 (package! org-edna)
 
-(package! websocket)
-(package! org-roam-ui
-  :recipe (:host github
-           :repo "org-roam/org-roam-ui"
-           :files ("*.el" "out")))
+;; (package! websocket)
+;; (package! org-roam-ui
+;;   :recipe (:host github
+;;            :repo "org-roam/org-roam-ui"
+;;            :files ("*.el" "out")))
 
 (package! ob-mermaid
   :disable t)
