0a516df5e42aa498a7bcb21cf88ce991dada3c03
Stefan Lendl <git@stfl.dev>
2025-09-23 13:26:39 +0200

    changes from kondor on Di 23 Sep 2025 13:26:39 CEST

--- init.el
+++ init.el
@@ -121,7 +121,7 @@
        ;; +enchant
        ;;  +everywhere
        ;; )
-       grammar           ; tasing grammar mistake every you make
+       ;; grammar           ; tasing grammar mistake every you make
 
        :tools
        ;;ansible
