d97c0da86f3d5f82e6e60c5e3ae769aa9d3ebbd9
Stefan Lendl <git@stfl.dev>
2025-09-25 19:17:56 +0200

    changes from kondor on Do 25 Sep 2025 19:17:56 CEST

--- config.el
+++ config.el
@@ -1886,6 +1886,60 @@ org-default-priority is treated as lower than the same set value"
 
 (add-to-list 'auto-mode-alist '("\\.service\\'" . conf-space-mode))
 
+(defvar stfl/ssh-deploy-local-mappings nil
+  "global alist to store local to remote mappings (local-file . remote-path).")
+
+(defun stfl/ssh-deploy-clear-mappings ()
+  (interactive)
+  "Clear all ssh-deploy mappings and remove buffer-local variables."
+  ;; For each mapping, find open buffers and clear their local variables
+  (dolist (mapping stfl/ssh-deploy-local-mappings)
+    (let* ((local-file (car mapping))
+           (buffer (get-file-buffer local-file)))
+      (when buffer
+        (message "Unregistering ssh-deploy mapping from %s" buffer)
+        (with-current-buffer buffer
+          (setq-local ssh-deploy-root-local nil
+                      ssh-deploy-root-remote nil)))))
+  ;; Clear the global alist
+  (setq stfl/ssh-deploy-local-mappings nil)
+  (message "Cleared all ssh-deploy mappings"))
+
+(defun stfl/ssh-deploy-register-local-mapping (&optional remote-path)
+  (interactive (if current-prefix-arg
+                   (list nil)  ; Don't prompt when unregistering
+                 (list (expand-file-name
+                        (read-file-name "Remote path: " nil
+                                        (when (boundp 'ssh-deploy-root-remote)
+                                          ssh-deploy-root-remote))))))
+  (require 'ssh-deploy)
+  (let ((local-file (expand-file-name (buffer-file-name))))
+    (if current-prefix-arg
+        (progn
+          (message "Unregistering ssh-deploy for this buffer")
+          (setq-local ssh-deploy-root-local nil
+                      ssh-deploy-root-remote nil)
+          ;; Remove mapping from global alist
+          (setq stfl/ssh-deploy-local-mappings
+                (assoc-delete-all local-file stfl/ssh-deploy-local-mappings)))
+      (progn
+        (setq-local ssh-deploy-root-local local-file
+                    ssh-deploy-root-remote remote-path)
+        (message "registered ssh-deploy for this buffer to %s" ssh-deploy-root-remote)
+        ;; Add/update mapping in global alist
+        (setq stfl/ssh-deploy-local-mappings
+              (cons (cons local-file remote-path)
+                    (assoc-delete-all local-file stfl/ssh-deploy-local-mappings)))))))
+
+(after! ssh-deploy
+  (setq! ssh-deploy-async 1)
+
+  (map! :map ssh-deploy-menu-map
+        :leader
+        :prefix "r"
+        "l" #'stfl/ssh-deploy-register-local-mapping
+        "L" #'stfl/ssh-deploy-clear-mappings))
+
 (after! flycheck
   (map! :map flycheck-mode-map
         :leader
