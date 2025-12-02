7d9358be9ccfbc354608e8a9827320153e959769
Stefan Lendl <git@stfl.dev>
2025-12-02 15:48:26 +0100

    changes from kondor on Di 02 Dez 2025 15:48:26 CET

--- init.el
+++ init.el
@@ -123,7 +123,7 @@
         +icons)
        (spell             ; tasing you for misspelling mispelling
         +enchant
-        ;; +everywhere
+        +everywhere
         )
        ;; grammar           ; tasing grammar mistake every you make
 
