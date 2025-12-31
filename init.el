ddccf0e3c7e0970c802ad24df71953a491a8d2da
Stefan Lendl <git@stfl.dev>
2025-12-31 21:23:21 +0100

    changes from kondor on Mi 31 Dez 2025 21:23:21 CET

--- init.el
+++ init.el
@@ -230,12 +230,13 @@
        plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
        (python             ; beautiful is better than ugly
+        +lsp
+        +tree-sitter
         ;; +pyenv
         +poetry
-        +lsp
         +pyright      ; (requirs +lsp)     ; NOTE can use basedpyright
         ;; +conda
-        +tree-sitter)
+        )
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
        ;; (rest              ; Emacs as a REST client
